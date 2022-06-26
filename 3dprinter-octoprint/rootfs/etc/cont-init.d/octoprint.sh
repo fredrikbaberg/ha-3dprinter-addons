7589879214b348d94cad424d9973bca455d41ca9
Fredrik Baberg <fredrik.baberg@gmail.com>
2022-06-27 01:26:41 +0200

    Add debug logging

--- 3dprinter-octoprint/rootfs/etc/cont-init.d/octoprint.sh
+++ 3dprinter-octoprint/rootfs/etc/cont-init.d/octoprint.sh
@@ -4,13 +4,19 @@
 # s6-overlay docs: https://github.com/just-containers/s6-overlay
 # ==============================================================================
 
+whoami
+
 { # Check if OctoPrint is installed
+    bashio::log.info "Check OctoPrint version..."
     octoprint --version
 } || { # Otherwise install
     { # Check if Python is available
+        bashio::log.info "Check Python version..."
         python --version
     } || { # Otherwise install
+        bashio::log.info "Install Python..."
         python3 -m venv /data/python/octoprint
-    }   
+    }
+    bashio::log.info "Install OctoPrint..."
     pip install octoprint==$OCTOPRINT_VERSION
 }
