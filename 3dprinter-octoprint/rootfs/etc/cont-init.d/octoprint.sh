8a84c682751bbd8be596b241fcd68d1d7e1ff940
Fredrik Baberg <fredrik.baberg@gmail.com>
2022-06-27 18:41:31 +0200

    Update nginx configuration

--- 3dprinter-octoprint/rootfs/etc/cont-init.d/octoprint.sh
+++ 3dprinter-octoprint/rootfs/etc/cont-init.d/octoprint.sh
@@ -5,17 +5,12 @@
 # ==============================================================================
 
 { # Check if OctoPrint is installed
-    bashio::log.info "Check OctoPrint version..."
     octoprint --version
 } || { # Otherwise install
     { # Check if Python is available
-        bashio::log.info "Check Python version..."
         python --version
     } || { # Otherwise install
-        bashio::log.info "Install Python..."
         python3 -m venv /data/python/octoprint
     }
-    bashio::log.info "Install OctoPrint..."
     pip install octoprint==$OCTOPRINT_VERSION
 }
-bashio::log.info "OctoPrint ready..."
