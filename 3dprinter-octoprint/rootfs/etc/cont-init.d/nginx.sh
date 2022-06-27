4982f00eb8af52bfc73c1cfde94c09c0673c3b6c
Fredrik Baberg <fredrik.baberg@gmail.com>
2022-06-27 16:51:33 +0200

    Update nginx

--- 3dprinter-octoprint/rootfs/etc/cont-init.d/nginx.sh
+++ 3dprinter-octoprint/rootfs/etc/cont-init.d/nginx.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Prepare proxy
+# Prepare nginx proxy
 # s6-overlay docs: https://github.com/just-containers/s6-overlay
 # ==============================================================================
 
-bashio::log.info "Proxy cont-init.d"
+bashio::log.info "nginx cont-init.d"
 
 ingress_entry=$(bashio::addon.ingress_entry)
 export ingress_entry=${ingress_entry}
+
 
 tempio \
     -conf /data/options.json \
     -template /usr/share/tempio/ingress.conf \
     -out /etc/nginx/sites-enabled/ingress.conf
+
+
+tempio \
+    -conf /data/options.json \
+    -template /usr/share/tempio/webui.conf \
+    -out /etc/nginx/sites-enabled/webui.conf
