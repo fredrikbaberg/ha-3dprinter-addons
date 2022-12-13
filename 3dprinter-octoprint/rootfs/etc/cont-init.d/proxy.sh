a9259cb33b67b560a5bb24693f998d2ce36f991c
Fredrik Baberg <fredrik.baberg@gmail.com>
2022-12-13 23:35:56 +0100

    Revert to single Caddyfile

--- 3dprinter-octoprint/rootfs/etc/cont-init.d/proxy.sh
+++ 3dprinter-octoprint/rootfs/etc/cont-init.d/proxy.sh
@@ -12,14 +12,5 @@ bashio::var.json \
     camera_host "$(bashio::config 'camera_url')" \
     mode "$(bashio::config 'mode')" \
     | tempio \
-        -template /usr/share/tempio/caddy/Caddyfile.ingress.gtpl \
-        -out /etc/caddy/configs/ingress.caddy
-
-# Generate proxy configuration for direct access, if enabled.
-if bashio::var.has_value "$(bashio::addon.port 5000)"; then
-    bashio::var.json \
-        mode "$(bashio::config 'mode')" \
-        | tempio \
-            -template /usr/share/tempio/caddy/Caddyfile.direct.gtpl \
-            -out /etc/caddy/configs/direct.caddy
-fi
+        -template /usr/share/tempio/caddy/Caddyfile.gtpl \
+        -out /etc/caddy/Caddyfile
