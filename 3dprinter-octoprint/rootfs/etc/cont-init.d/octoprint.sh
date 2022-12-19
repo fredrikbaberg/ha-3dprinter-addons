6729d5684a28827c5f70e66d3fc85c5634221655
Fredrik Baberg <fredrik.baberg@gmail.com>
2022-12-19 14:15:55 +0100

    Revert OctoPrint version; Add script to update config.

--- 3dprinter-octoprint/rootfs/etc/cont-init.d/octoprint.sh
+++ 3dprinter-octoprint/rootfs/etc/cont-init.d/octoprint.sh
@@ -28,12 +28,12 @@ if [ ! -f /data/config/octoprint/config.yaml ]; then
     fi
 fi
 
-{ # Make sure Ingress user for OctoPrint exists.
-    bashio::log.notice "Ensure Ingress user (homeassistant) exist."
-    if ! octoprint --basedir /data/config/octoprint user list | grep -q 'homeassistant'; then
-        new_password=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
-        octoprint --basedir /data/config/octoprint user add --password "$new_password" --admin homeassistant # 2> /dev/null
-    fi
-} || { # catch
-    bashio::log.warning "Failed to ensure Ingress user exists, may not be able to launch."
-}
+# { # Make sure Ingress user for OctoPrint exists.
+#     bashio::log.notice "Ensure Ingress user (homeassistant) exist."
+#     if ! octoprint --basedir /data/config/octoprint user list | grep -q 'homeassistant'; then
+#         new_password=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+#         octoprint --basedir /data/config/octoprint user add --password "$new_password" --admin homeassistant # 2> /dev/null
+#     fi
+# } || { # catch
+#     bashio::log.warning "Failed to ensure Ingress user exists, may not be able to launch."
+# }
