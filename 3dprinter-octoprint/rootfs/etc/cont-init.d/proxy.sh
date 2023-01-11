1dde43aa226ccf5f05de9d58ce6520fdcd3a2e8f
Fredrik Baberg <fredrik.baberg@gmail.com>
2023-01-11 16:42:45 +0100

    Single file for internal and external Caddyfile

--- 3dprinter-octoprint/rootfs/etc/cont-init.d/proxy.sh
+++ 3dprinter-octoprint/rootfs/etc/cont-init.d/proxy.sh
@@ -4,20 +4,14 @@
 # Configures proxy
 # ==============================================================================
 
-# Generate proxy configuration for internal access
+# Generate proxy configuration for internal and external access
 bashio::var.json \
     addon_hostname "$(bashio::addon.hostname)" \
     addon_ip "$(bashio::addon.ip_address)" \
-    | tempio \
-        -template /usr/share/tempio/caddy/Caddyfile.internal.gtpl \
-        -out /etc/caddy/sites-enabled/internal.caddy
-
-# Generate proxy configuration for external access
-bashio::var.json \
     trusted_proxies "$(bashio::config 'trusted_proxies')" \
     | tempio \
-        -template /usr/share/tempio/caddy/Caddyfile.external.gtpl \
-        -out /etc/caddy/sites-enabled/external.caddy
+        -template /usr/share/tempio/caddy/Caddyfile.internal_external.gtpl \
+        -out /etc/caddy/sites-enabled/internal_external.caddy
 
 # Generate proxy configuration for access by Ingress
 # bashio::var.json \
--- 3dprinter-octoprint/rootfs/usr/share/tempio/caddy/Caddyfile.internal.gtpl
+++
@@ -1,6 +0,0 @@
-# Internal access, e.g. for Home-Assistant OctoPrint integration.
-:5000 {
-	bind {{ .addon_ip }}
-	reverse_proxy http://127.0.0.1:80 {
-	}
-}
