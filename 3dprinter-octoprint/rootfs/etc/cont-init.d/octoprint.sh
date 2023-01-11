fa9d7b18372a74f9f6f18ae35f482d1bac818d4b
Fredrik Baberg <fredrik.baberg@gmail.com>
2023-01-11 12:20:46 +0100

    Only set host and port in launch argument, in case config is exported

--- 3dprinter-octoprint/rootfs/etc/cont-init.d/octoprint.sh
+++ 3dprinter-octoprint/rootfs/etc/cont-init.d/octoprint.sh
@@ -29,8 +29,6 @@ updateConfigRequired()
     octoprint $BASEDIR config set server.commands.serverRestartCommand "/scripts/octoprint_restart.sh"
     octoprint $BASEDIR config set server.commands.systemRestartCommand "/scripts/system_restart.sh"
     octoprint $BASEDIR config set server.commands.systemShutdownCommand "/scripts/system_shutdown.sh"
-    octoprint $BASEDIR config set server.host "127.0.0.1"
-    octoprint $BASEDIR config set --int server.port 80
     octoprint $BASEDIR config set webcam.ffmpeg "/usr/bin/ffmpeg"
 }
 
