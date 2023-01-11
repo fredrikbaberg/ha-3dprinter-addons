dcc46a98f2ef84995699b0582970d122395507de
Fredrik Baberg <fredrik.baberg@gmail.com>
2023-01-11 14:55:57 +0100

    Skip Ingress code

--- 3dprinter-octoprint/rootfs/etc/cont-init.d/proxy.sh
+++ 3dprinter-octoprint/rootfs/etc/cont-init.d/proxy.sh
@@ -5,19 +5,19 @@
 # ==============================================================================
 
 # Generate proxy configuration for access by Ingress
-bashio::var.json \
-    addon_hostname "$(bashio::addon.hostname)" \
-    addon_ip "$(bashio::addon.ip_address)" \
-    camera_host "$(bashio::config 'camera_url')" \
-    ingress_entry "$(bashio::addon.ingress_entry)" \
-    ingress_port "^$(bashio::addon.ingress_port)" \
-    interface "$(bashio::addon.ip_address)" \
-    mode "$(bashio::config 'mode')" \
-    supervisor_ip "$(bashio::supervisor.ip_address)" \
-    trusted_proxies "$(bashio::config 'trusted_proxies')" \
-    | tempio \
-        -template /usr/share/tempio/caddy/Caddyfile.ingress.gtpl \
-        -out /etc/caddy/sites-enabled/ingress.caddy
+# bashio::var.json \
+#     addon_hostname "$(bashio::addon.hostname)" \
+#     addon_ip "$(bashio::addon.ip_address)" \
+#     camera_host "$(bashio::config 'camera_url')" \
+#     ingress_entry "$(bashio::addon.ingress_entry)" \
+#     ingress_port "^$(bashio::addon.ingress_port)" \
+#     interface "$(bashio::addon.ip_address)" \
+#     mode "$(bashio::config 'mode')" \
+#     supervisor_ip "$(bashio::supervisor.ip_address)" \
+#     trusted_proxies "$(bashio::config 'trusted_proxies')" \
+#     | tempio \
+#         -template /usr/share/tempio/caddy/Caddyfile.ingress.gtpl \
+#         -out /etc/caddy/sites-enabled/ingress.caddy
 
 # Generate proxy configuration for any access
 # tempio \
@@ -32,4 +32,4 @@ bashio::var.json \
 #         -out /etc/caddy/sites-enabled/internal.caddy
 
 # Make sure file is correctly formatted.
-caddy fmt --overwrite /etc/caddy/Caddyfile
+# caddy fmt --overwrite /etc/caddy/Caddyfile
